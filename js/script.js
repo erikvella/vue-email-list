36f03b92b18667aca57017d6dcaa8a34473b9beb
erikvella <vella.erik1@gmail.com>
2023-10-06 15:41:43 +0100

    [add] 1mail

--- js/script.js
+++ js/script.js
@@ -4,8 +4,27 @@ createApp({
 
   data(){
     return{
-      
+
+      apiUrl:'https://flynn.boolean.careers/exercises/api/random/mail',
+      mailGenerate: ''
     }
+  },
+
+  methods:{
+    getApi(){
+    axios.get(this.apiUrl)
+    .then((response)=>{
+      console.log(response.data);
+      this.mailGenerate = response.data.response
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+    }
+  },
+
+  mounted(){
+    this.getApi();
   }
 
 }).mount('#app');
